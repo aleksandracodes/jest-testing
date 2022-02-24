5a9db846df06ce40c23dd5fed9d8be214ece75ec
Aleksandra Haniok <a.haniok@gmail.com>
2022-02-24 11:28:10 +0000

    Add function to read index.html file

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,7 +5,11 @@
  const buttonClick = require("../button");
 
  beforeEach(() => {
-     document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
  });
  
  describe("DOM tests", () => {
@@ -13,4 +17,9 @@
          buttonClick();
          expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
      });
+     test("h1 should exist", () => {
+        buttonClick();
+        // if the 1x h1 exists then the length will be 1, if 2 x h1 then length = 2
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
  });
