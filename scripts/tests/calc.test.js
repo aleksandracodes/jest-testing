3f892aa4c4520a1cf42f546461b5ef0c6b9668e1
Aleksandra Haniok <a.haniok@gmail.com>
2022-02-24 11:03:42 +0000

    Improve the function by adding 2nd test

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,10 @@ describe("Calculator", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
         });
+        test("should return 17 for 7 + 10", () => {
+            expect(addition(7, 10)).toBe(17);
+        });
+        
     });
     describe("Subtract function", () => {
 
